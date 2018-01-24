bb62a94225963b467bb5a04231cdb5c23456f54b
Near Lg <nearlg@gmail.com>
2018-01-24 13:02:34 +0100

    Fix: remove unnecessary files

--- install.sh
+++ install.sh
@@ -8,9 +8,3 @@ systemctl daemon-reload
 systemctl start gpio-control-relay
 systemctl status gpio-control-relay
 echo "Installation done!"
-
-#cp ${DIR}/service.sh /etc/init.d/gpio-control-relay.sh
-#chmod ug+x /etc/init.d/gpio-control-relay.sh
-#update-rc.d gpio-control-relay.sh defaults
-#mkdir -p /usr/lib/gpio-control-relay/
-#cp -r ${DIR} /usr/lib/
