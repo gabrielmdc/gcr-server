df7fc8e1f44c3312ee09488dc90fe5dc69b158cc
Near Lg <nearlg@gmail.com>
2018-05-04 23:50:35 +0200

    Fix: instalation script

--- install.sh
+++ install.sh
@@ -3,7 +3,13 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-cp ${DIR}/gcr-server.service /etc/systemd/system/
+if [ -d "/usr/lib/gcr-server" ]; then
+    cp -r ${DIR}/. /usr/lib/gcr-server
+else
+    cp -r ${DIR} /usr/lib/gcr-server
+fi
+
+cp /usr/lib/gcr-server/gcr-server.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl start gcr-server
 systemctl status gcr-server
